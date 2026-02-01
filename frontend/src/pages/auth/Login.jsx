26e216316166ca425d28dc0e760d0f0dc2be365c
Divyansh Sharma <divyanshsharma151020@gmail.com>
2026-02-01 14:25:29 +0530

    Add autoComplete= off to prevent the automatically filled the previous filled login credentialls

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -17,12 +17,12 @@ const Login = ()=>{
   }
   return(
     <div className="flex items-center justify-center min-h-screen"> 
-      <form onSubmit={handleSubmit} className="bg-white text-gray-500 max-w-87.5 mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10">
+      <form onSubmit={handleSubmit} autoComplete="off" className="bg-white text-gray-500 max-w-87.5 mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10">
             <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">Login Now</h2>
 
-            <input id="email" name="email" onChange={handleChange} value={loginFormData.email} className="w-full border my-3 border-gray-500/30 outline-none rounded-full py-2.5 px-4" type="email" placeholder="Enter your email" required />
+            <input id="email" name="email" autoComplete="off" onChange={handleChange} value={loginFormData.email} className="w-full border my-3 border-gray-500/30 outline-none rounded-full py-2.5 px-4" type="email" placeholder="Enter your email" required />
 
-            <input id="password" name="password" onChange={handleChange} value={loginFormData.password} className="w-full border mt-1 border-gray-500/30 outline-none rounded-full py-2.5 px-4" type="password" placeholder="Enter your password" required />
+            <input id="password" name="password" autoComplete="new-password" onChange={handleChange} value={loginFormData.password} className="w-full border mt-1 border-gray-500/30 outline-none rounded-full py-2.5 px-4" type="password" placeholder="Enter your password" required />
 
             <div className="text-right py-4">
                 <a className="text-blue-600 underline" href="#">Forgot Password</a>
